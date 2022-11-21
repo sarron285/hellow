cf95def4fa89b87c55e0b0084c3016c1b0cda484
sarron285 <sarron285@alu.centroafuera.es>
2022-11-21 10:47:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/hellow/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hellow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
